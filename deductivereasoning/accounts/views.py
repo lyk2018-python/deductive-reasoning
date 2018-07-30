1f08fc281abe6001ddd9bdd37f990a6ba691ae63
Ahmet,Korkmaz <ahmetkorkmaz3453@gmail.com>
2018-07-30 12:26:57 +0300

    Fix Base.html

--- deductivereasoning/accounts/views.py
+++ deductivereasoning/accounts/views.py
@@ -9,7 +9,7 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
+            raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('home')
