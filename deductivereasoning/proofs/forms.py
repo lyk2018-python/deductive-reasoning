221e2f10a4dd3dbacd38c0156677ce469e758a14
bulentkablan <bulent.kablan@ceng.ktu.edu.tr>
2018-07-30 15:50:28 +0300

    Change views

--- deductivereasoning/proofs/forms.py
+++ deductivereasoning/proofs/forms.py
@@ -1,32 +1,32 @@
 from django import forms
 
 class MajorSubmissionForm(forms.Form):
-    is_universal_major = forms.BooleanField(required=True)
+    is_universal_major = forms.BooleanField(required=False)
     subject_major = forms.CharField(
         max_length=255,
         label='Major subject',
     )
-    is_affirmative_major = forms.BooleanField(required=True)
+    is_affirmative_major = forms.BooleanField(required=False)
     predicate_major = forms.CharField(
         max_length=255,
         label='Major predicate',
     )
-    is_universal_minor = forms.BooleanField(required=True)
+    is_universal_minor = forms.BooleanField(required=False)
     subject_minor = forms.CharField(
         max_length=255,
         label='Major subject',
     )
-    is_affirmative_minor = forms.BooleanField(required=True)
+    is_affirmative_minor = forms.BooleanField(required=False)
     predicate_minor = forms.CharField(
         max_length=255,
         label='Major predicate',
     )
-    is_universal_conclusion = forms.BooleanField(required=True)
+    is_universal_conclusion = forms.BooleanField(required=False)
     subject_conclusion = forms.CharField(
         max_length=255,
         label='Major subject',
     )
-    is_affirmative_conclusion = forms.BooleanField(required=True)
+    is_affirmative_conclusion = forms.BooleanField(required=False)
     predicate_conclusion = forms.CharField(
         max_length=255,
         label='Major predicate',
--- deductivereasoning/proofs/views.py
+++ deductivereasoning/proofs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from proofs.models import Proposition, Proof
-from .forms import MajorSubmissionForm 
+from .forms import MajorSubmissionForm
 
 # Create your views here.
 def home(request):
@@ -14,6 +14,33 @@ def home(request):
 
 def submit(request):
 	form = MajorSubmissionForm()
-	
+
+	if request.method == "POST":
+		form = MajorSubmissionForm(request.POST)
+
+		if form.is_valid():
+			major = Proposition.objects.create(
+			is_universal=form.cleaned_data['is_universal_major'],
+			subject=form.cleaned_data['subject_major'],
+			is_affirmative=form.cleaned_data['is_affirmative_major'],
+			predicate=form.cleaned_data['predicate_major'],
+			)
+			minor = Proposition.objects.create(
+			is_universal=form.cleaned_data['is_universal_minor'],
+			subject=form.cleaned_data['subject_minor'],
+			is_affirmative=form.cleaned_data['is_affirmative_minor'],
+			predicate=form.cleaned_data['predicate_minor'],
+			)
+			conclusion = Proposition.objects.create(
+			is_universal=form.cleaned_data['is_universal_conclusion'],
+			subject=form.cleaned_data['subject_conclusion'],
+			is_affirmative=form.cleaned_data['is_affirmative_conclusion'],
+			predicate=form.cleaned_data['predicate_conclusion'],
+			)
+			#proof_major = Proof.objects.create(major)
+			#proof_minor = Proof.objects.create(minor)
+			#proof_conclusion = Proof.objects.create(conclusion)
+
+
 
 	return render(request ,"submit.html", {'form': form})
