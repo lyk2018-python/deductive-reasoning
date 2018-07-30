994ffbfb6532934deb15a6e0b4425f1b4ecb7245
ismaildenizli <schooldaysvp@gmail.com>
2018-07-30 03:11:54 +0300

    Add typeof proposition

--- deductivereasoning/proofs/models.py
+++ deductivereasoning/proofs/models.py
@@ -9,10 +9,21 @@ class Proposition(models.Model):
     subject = models.CharField(max_length=30)
     is_affirmative = models.BooleanField()
     predicate = models.CharField(max_length=30)
+    type = models.CharField(max_length=5,default="",blank=True)
+
+
+    def setPropositionType(self):
+        if(self.is_universal==True and self.is_affirmative==True):
+            self.type = "A"
+        elif(self.is_universal==True and self.is_affirmative==False):
+            self.type = "I"
+        elif (self.is_universal == False and self.is_affirmative == True):
+            self.type = "E"
+        else:
+            self.type = "O"
 
     def __str__(self):
         return  str(self.is_universal) + " " + self.subject + " " + str(self.is_affirmative) + " " + self.predicate
-
 
 class Proof(models.Model):
     """
@@ -33,6 +44,7 @@ class Proof(models.Model):
         related_name='conclusion',
         on_delete=models.CASCADE,
     )
+    type = models.CharField(max_length=1,default="")
 
     def __str__(self):
         return  self.major.subject + " " + self.major.predicate + "/n" + self.minor.subject + " " + self.minor.predicate + "/n" + self.conclusion.subject + " " + self.conclusion.predicate
