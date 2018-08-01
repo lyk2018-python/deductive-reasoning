21c4a5662f8f10f6ec761e4d3030b3e3290a1132
omerbselvi <omerbugra97@hotmail.com>
2018-08-01 16:17:36 +0300

    Add navbar, dynamic choicefields

--- deductivereasoning/proofs/forms.py
+++ deductivereasoning/proofs/forms.py
@@ -10,25 +10,22 @@ class MajorSubmissionForm(forms.Form):
     is_affirmative_major = forms.BooleanField(required=False)
     predicate_major = forms.CharField(
         max_length=255,
-        label='Major predicate',
+        label='Major predicate',required=False
     )
     is_universal_minor = forms.BooleanField(required=False)
     subject_minor = forms.CharField(
         max_length=255,
-        label='Major subject',
+        label='Major subject',required=False
     )
     is_affirmative_minor = forms.BooleanField(required=False)
     predicate_minor = forms.CharField(
         max_length=255,
-        label='Major predicate',
+        label='Major predicate',required=False
     )
     is_universal_conclusion = forms.BooleanField(required=False)
-    subject_conclusion = forms.CharField(
-        max_length=255,
-        label='Major subject',
-    )
+    subject_conclusion = forms.ChoiceField(
+        label='Major subject',required=False    )
     is_affirmative_conclusion = forms.BooleanField(required=False)
-    predicate_conclusion = forms.CharField(
-        max_length=255,
-        label='Major predicate',
+    predicate_conclusion = forms.ChoiceField(
+        label='Major predicate',required=False
     )
